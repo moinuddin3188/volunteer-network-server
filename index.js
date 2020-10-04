6d07bbbdb4251a41bc402778a1f72fdd5f775d77
mohi-uddin-bff <moinuddin3188@gmail.com>
2020-10-05 01:20:43 +0600

    first

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 const objectID = require('mongodb').ObjectID;
 require('dotenv').config()
 
-
+ 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkovl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
